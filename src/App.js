f9875e0518e30fc3ff52df581943b4fdc61c38de
wonho-jung <wonho.jung92@gmail.com>
2021-02-11 16:35:36 -0500

    sidebar(profile)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header/Header";
+import Sidebar from "./Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="app">
       {/* HEADER */}
       <Header />
+      <div className="app__body">
+        <Sidebar />
+      </div>
       {/* App Body */}
       {/* Sidebar */}
       {/* Feed */}
---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,33 @@
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://images.unsplash.com/photo-1501630834273-4b5604d2ee31?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h2>wonho Jung</h2>
+        <h4>wonho.jung92@gmail.com</h4>
+      </div>
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,543</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">2,543</p>
+        </div>
+      </div>
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
